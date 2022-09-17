5e9b313f52039196a1c14873f429de6af8467b5d
jacky32 <patrki@atlas.cz>
2022-09-17 22:44:07 +0200

    Fix login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: 'events#index'
 
-  # resources :users
-  resources :events, only: [:index]
+  resources :users
+  resources :events, only: %i[index new]
 end
