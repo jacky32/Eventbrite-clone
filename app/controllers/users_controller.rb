b814bf952bdda6132084fdf1d3272154f54d9c75
jacky32 <patrki@atlas.cz>
2022-09-22 16:00:08 +0200

    Add show page for users that lists their events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def show
+    user
+    @organised_events = user.events
+  end
+
+  private
+
+  def user
+    @user ||= User.find(params[:id])
+  end
 end
