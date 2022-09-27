025ba3a5fef005b6dcab59a0dfb7f31221d4e19e
jacky32 <patrki@atlas.cz>
2022-09-27 11:21:12 +0200

    Move bulma-calendar to separate file

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "bulma-calendar", to: "https://ga.jspm.io/npm:bulma-calendar@6.1.18/dist/js/bulma-calendar.js"
+pin 'application', preload: true
+pin 'bulma-calendar', to: 'https://ga.jspm.io/npm:bulma-calendar@6.1.18/dist/js/bulma-calendar.js'
+pin 'calendar'
