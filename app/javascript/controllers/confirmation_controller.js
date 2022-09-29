1e376dbfc25704a3fcb9e8ba31515c5e9ba736ab
jacky32 <patrki@atlas.cz>
2022-09-29 11:41:15 +0200

    Remove unused method

--- app/javascript/controllers/confirmation_controller.js
+++ app/javascript/controllers/confirmation_controller.js
@@ -1,13 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  confirm(event) {
-    if (!window.confirm(this.messageValue)) {
-      event.preventDefault();
-      event.stopImmediatePropagation();
-    }
-  }
-
   toggleModal(event) {
     event.preventDefault();
     const deleteModal = document.getElementById("confirmation-modal");
