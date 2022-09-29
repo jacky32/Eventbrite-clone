3868a02055d43bc3356143f7264be2a6fe74bb02
jacky32 <patrki@atlas.cz>
2022-09-29 11:40:14 +0200

    Add delete event modal

--- app/javascript/controllers/confirmation_controller.js
+++ app/javascript/controllers/confirmation_controller.js
@@ -1,12 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static values = { message: String };
-
   confirm(event) {
     if (!window.confirm(this.messageValue)) {
       event.preventDefault();
       event.stopImmediatePropagation();
     }
+  }
+
+  toggleModal(event) {
+    event.preventDefault();
+    const deleteModal = document.getElementById("confirmation-modal");
+    deleteModal.classList.toggle("is-active");
   }
 }
